0c383042d2a318dc49363a80727fadc1010a7c57
xclawkevin <34619818+xclawkevin@users.noreply.github.com>
2021-04-06 02:19:03 +0700

    Update eggPriceContracts.ts

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,5 +1,5 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+  cakeAddress: '0xa184088a740c695e156f91f5cc086a06bb78b827',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
 }
